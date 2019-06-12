86f09f14e77344f13b8fe6756a7f0bf5f5b0a065
unknown <stats.inquartic@outlook.com>
2019-06-12 16:24:16 +0100

    connection changes

--- src/testUtils/createTestConn.ts
+++ src/testUtils/createTestConn.ts
@@ -1,11 +1,47 @@
-import { getConnectionOptions, createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 
 export const createTestConn = async (resetDB: boolean = false) => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({
-    ...connectionOptions,
-    name: "default",
-    synchronize: resetDB,
-    dropSchema: resetDB
-  });
+
+  if (process.env.NODE_ENV == "test") {
+    return createConnection({
+      "type": "mysql",
+      "host": "host",
+      "port": 3306,
+      "username": "username",
+      "password": "password",
+      "database": "freedom_test",
+      "synchronize": resetDB,
+      "logging": false,
+      "dropSchema": resetDB,
+      "entities": ["src/entity/**/*.ts"],
+      "migrations": ["src/migration/**/*.ts"],
+      "subscribers": ["src/subscriber/**/*.ts"],
+      "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+      }
+    });
+  } else if (process.env.NODE_ENV == "developement") {
+    return createConnection({
+      "type": "mysql",
+      "host": "host",
+      "port": 3306,
+      "username": "username",
+      "password": "password",
+      "database": "freedom",
+      "synchronize": true,
+      "logging": true,
+      "entities": ["src/entity/**/*.ts"],
+      "migrations": ["src/migration/**/*.ts"],
+      "subscribers": ["src/subscriber/**/*.ts"],
+      "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+      }
+    });
+  } else {
+    return null
+  }
 };
--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,6 +1,47 @@
-import { getConnectionOptions, createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 
 export const createTypeormConn = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: "default" });
+  if (process.env.NODE_ENV == "test") {
+    return createConnection({
+      "type": "mysql",
+      "host": "host",
+      "port": 3306,
+      "username": "username",
+      "password": "password",
+      "database": "freedom_test",
+      "synchronize": true,
+      "logging": false,
+      "dropSchema": true,
+      "entities": ["src/entity/**/*.ts"],
+      "migrations": ["src/migration/**/*.ts"],
+      "subscribers": ["src/subscriber/**/*.ts"],
+      "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+      }
+    });
+  } else if (process.env.NODE_ENV == "developement") {
+    return createConnection({
+      "type": "mysql",
+      "host": "host",
+      "port": 3306,
+      "username": "username",
+      "password": "password",
+      "database": "freedom",
+      "synchronize": true,
+      "logging": true,
+      "entities": ["src/entity/**/*.ts"],
+      "migrations": ["src/migration/**/*.ts"],
+      "subscribers": ["src/subscriber/**/*.ts"],
+      "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+      }
+    });
+  } else {
+    return null
+  }
+  
 };
